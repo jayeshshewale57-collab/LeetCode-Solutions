833d41ccd8510c142bbb55456a7cc1209e3be0d0
Jayesh Shewale <jayeshshewale57@gmail.com>
2026-09-20 10:49:40 +0530

    Refactor removeNthFromEnd method in Leetcode19

--- Leetcode19.java
+++ Leetcode19.java
@@ -10,16 +10,7 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-
-        if(head==null){
-            return null;
-        }
-
-        if(head.next==null){
-            head=null;
-            return null;
-        }
-
+        
         int i=0,count=0;
         ListNode temp=head;
 
